d47ff3fe036d154d358788106476d8dcf5bea98c
khassanein <khassanein@jahez.net>
2026-05-02 17:51:30 +0300

    run remote on ci jobs

--- src/test/java/RecruitmentTest.java
+++ src/test/java/RecruitmentTest.java
@@ -1,3 +1,4 @@
+import org.hrm.DashboardPage;
 import org.hrm.RecruitmentPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,9 +13,9 @@ public class RecruitmentTest extends TestBase {
 
     @Test
     public void checkThatRecruitmentIsOpened() {
-        System.out.println("Session ID: " + LocalDateTime.now().toString());
         System.out.println("Requirement time now is : " + new Date().getTime());
         recruitmentPage = new RecruitmentPage(webDriver);
+        DashboardPage dashboardPage = new DashboardPage(webDriver);
         dashboardPage.openRecruitmentPage();
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates");
     }
