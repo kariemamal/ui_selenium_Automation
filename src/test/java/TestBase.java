27437eaf21eef66fea5613f9075fa186cacbb202
khassanein <khassanein@jahez.net>
2026-05-02 15:23:55 +0300

    run remote on ci jobs

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -56,6 +56,11 @@ public class TestBase {
     private WebDriver createLocalDriver() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
         return new ChromeDriver(options);
     }
 
